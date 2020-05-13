f05d9c86d7c54466468eefd471a6a2d10bfbd27a
zlum <mr.zlum@gmail.com>
2020-05-14 01:21:48 +0300

    Added some tests.

--- test/test.cpp
+++ test/test.cpp
@@ -12,41 +12,38 @@ using namespace bigNumber;
 
 TEST(CharStrInput, BigNumber)
 {
-    BigNumberBuilder bnb1;
-    BigNumberBuilder bnb2;
+    BigNumberBuilder bnb;
 
-    bnb1.appendChar('9');
-    bnb1.appendChar('8');
-    bnb1.appendChar('0');
-    bnb1.appendChar('4');
-    bnb1.appendChar('1');
-    bnb1.appendChar(',');
-    bnb1.appendChar('7');
-    bnb1.appendChar('9');
-    bnb1.appendChar('9');
-    bnb1.appendChar('3');
-    bnb1.appendChar('0');
-    bnb1.appendChar('0');
-    bnb1.appendChar('0');
+    bnb.appendChar('9');
+    bnb.appendChar('8');
+    bnb.appendChar('0');
+    bnb.appendChar('4');
+    bnb.appendChar('1');
+    bnb.appendChar(',');
+    bnb.appendChar('7');
+    bnb.appendChar('9');
+    bnb.appendChar('9');
+    bnb.appendChar('3');
+    bnb.appendChar('0');
+    bnb.appendChar('0');
+    bnb.appendChar('0');
+    BigNumber test1{bnb.build()};
 
-    bnb2.appendStr("98041.7993");
-
-    BigNumber test1{bnb1.build()};
-    BigNumber test2{bnb2.build()};
+    bnb.appendStr("98041.7993");
+    BigNumber test2{bnb.build()};
 
     EXPECT_EQ(test1, test2);
 }
 
 TEST(SumDiff, BigNumber)
 {
-    BigNumberBuilder bnb1;
-    BigNumberBuilder bnb2;
+    BigNumberBuilder bnb;
 
-    bnb1.appendStr("98041.7993");
-    bnb2.appendStr("680.4");
+    bnb.appendStr("98041.7993");
+    BigNumber test1{bnb.build()};
 
-    BigNumber test1{bnb1.build()};
-    BigNumber test2{bnb2.build()};
+    bnb.appendStr("680.4");
+    BigNumber test2{bnb.build()};
 
     BigNumber testSum{test1 + test2};
 
@@ -55,14 +52,13 @@ TEST(SumDiff, BigNumber)
 
 TEST(ProdQuat, BigNumber)
 {
-    BigNumberBuilder bnb1;
-    BigNumberBuilder bnb2;
+    BigNumberBuilder bnb;
 
-    bnb1.appendStr("98041.7993");
-    bnb2.appendStr("680.4");
+    bnb.appendStr("98041.7993");
+    BigNumber test1{bnb.build()};
 
-    BigNumber test1{bnb1.build()};
-    BigNumber test2{bnb2.build()};
+    bnb.appendStr("680.4");
+    BigNumber test2{bnb.build()};
 
     BigNumber testProd{test1 * test2};
 
@@ -71,30 +67,145 @@ TEST(ProdQuat, BigNumber)
 
 TEST(SumProd, BigNumber)
 {
-    BigNumberBuilder bnb1;
-    BigNumberBuilder bnb2;
+    BigNumberBuilder bnb;
 
-    bnb1.appendStr("98041.7993");
-    bnb2.appendStr("3");
+    bnb.appendStr("98041.7993");
+    BigNumber test1{bnb.build()};
 
-    BigNumber test1{bnb1.build()};
-    BigNumber test2{bnb2.build()};
+    bnb.appendStr("3");
+    BigNumber test2{bnb.build()};
 
     EXPECT_EQ(test1 + test1 + test1, test1 * test2);
 }
 
+TEST(BigIntegerSum, BigNumber)
+{
+    BigNumberBuilder bnb;
+
+    bnb.appendStr("99999999999999999999999999999999999999999999999999999");
+    BigNumber test1{bnb.build()};
+
+    bnb.appendStr("1");
+    BigNumber test2{bnb.build()};
+
+    bnb.appendStr("100000000000000000000000000000000000000000000000000000");
+    BigNumber test3{bnb.build()};
+
+    EXPECT_EQ(test1 + test2, test3);
+}
+
+TEST(BigFractialSum, BigNumber)
+{
+    BigNumberBuilder bnb;
+
+    bnb.appendStr("0.99999999999999999999999999999999999999999999999999999");
+    BigNumber test1{bnb.build()};
+
+    bnb.appendStr("0.00000000000000000000000000000000000000000000000000001");
+    BigNumber test2{bnb.build()};
+
+    bnb.appendStr("1");
+    BigNumber test3{bnb.build()};
+
+    EXPECT_EQ(test1 + test2, test3);
+}
+
+TEST(BigFractialRound, BigNumber)
+{
+    // Precision = 30
+    BigNumberBuilder bnb;
+
+    bnb.appendStr("37598041.799299999999999999999999999999999999999999999998");
+    BigNumber test1{bnb.build()};
+
+    bnb.appendStr("37598041.7993");
+    BigNumber test2{bnb.build()};
+
+    bnb.appendStr("11.234562222222222222222222222223");
+    BigNumber test3{bnb.build()};
+
+    bnb.appendStr("11.2345622222222222222222222222234");
+    BigNumber test4{bnb.build()};
+
+    EXPECT_EQ(test1.round(), test2);
+    EXPECT_EQ(test1.round(), test1.round());
+    EXPECT_EQ(test3.round(), test4.round());
+}
+
+TEST(Less, BigNumber)
+{
+    BigNumberBuilder bnb;
+
+    bnb.appendStr("37598041.799299999999999999999999999999999999999999999998");
+    BigNumber test1{bnb.build()};
+
+    bnb.appendStr("37598041.7993");
+    BigNumber test2{bnb.build()};
+
+    bnb.appendStr("7993");
+    BigNumber test3{bnb.build()};
+    bnb.appendStr("799299999999999999999999999999999999999999999998");
+    BigNumber test4{bnb.build()};
+
+    bnb.appendStr("0.0000000000000000000000000000000000000000007993");
+    BigNumber test5{bnb.build()};
+    bnb.appendStr("799299999999999999999999999999999999999999999998");
+    BigNumber test6{bnb.build()};
+
+    EXPECT_TRUE(test1 < test2);
+    EXPECT_TRUE(test3 < test4);
+    EXPECT_TRUE(test5 < test6);
+}
+
+TEST(Greater, BigNumber)
+{
+    BigNumberBuilder bnb;
+
+    bnb.appendStr("37598041.799299999999999999999999999999999999999999999998");
+    BigNumber test1{bnb.build()};
+
+    bnb.appendStr("37598041.7993");
+    BigNumber test2{bnb.build()};
+
+    bnb.appendStr("7993");
+    BigNumber test3{bnb.build()};
+    bnb.appendStr("799299999999999999999999999999999999999999999998");
+    BigNumber test4{bnb.build()};
+
+    bnb.appendStr("0.0000000000000000000000000000000000000000007993");
+    BigNumber test5{bnb.build()};
+    bnb.appendStr("799299999999999999999999999999999999999999999998");
+    BigNumber test6{bnb.build()};
+
+    EXPECT_TRUE(test2 > test1);
+    EXPECT_TRUE(test4 > test3);
+    EXPECT_TRUE(test6 > test5);
+}
+
 TEST(QuatSum, BigNumber)
 {
-    BigNumberBuilder bnb1;
-    BigNumberBuilder bnb2;
+    BigNumberBuilder bnb;
 
-    bnb1.appendStr("98041.7993");
-    bnb2.appendStr("3");
+    bnb.appendStr("98041.7993");
+    BigNumber test1{bnb.build()};
 
-    BigNumber test1{bnb1.build()};
-    BigNumber test2{bnb2.build()};
+    bnb.appendStr("3");
+    BigNumber test2{bnb.build()};
 
     BigNumber testQuat{test1 / test2};
 
     EXPECT_EQ(BigNumber(testQuat + testQuat + testQuat).round(), test1);
+}
+
+TEST(SqrtQuat, BigNumber)
+{
+    BigNumberBuilder bnb;
+
+    bnb.appendStr("1600000000000000000000");
+    BigNumber test1{bnb.build()};
+
+    bnb.appendStr("40000000000");
+    BigNumber test2{bnb.build()};
+
+    EXPECT_EQ(BigNumber(test1.sqrt()).round(), test1 / test2);
 }
