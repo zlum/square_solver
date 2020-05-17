20cdf4cd0009d2251e9e1b71bd18f9937d5d691f
zlum <mr.zlum@gmail.com>
2020-05-17 21:52:40 +0300

    Refactored buffer.

--- producerconsumer/buffer.hpp
+++ producerconsumer/buffer.hpp
@@ -17,67 +17,11 @@ public:
 
     // Wakes up every thread
     // Must be used after work flag change to interrupt operations
-    void notifyAll()
-    {
-        _queueEmptyCondVar.notify_all();
-        _queueFullCondVar.notify_all();
-    }
-
+    void notifyAll();
     // Thread-safely adds task to the queue
-    bool emplace(Task&& task, const bool& workFlag)
-    {
-        std::unique_lock<std::mutex> lock(_queueMtx);
-
-        // Hold thread if task limit had been reached
-        while(_taskQueue.size() >= _queueMaxSize)
-        {
-            // Interrupt operation if work flag is false
-            if(!workFlag)
-            {
-                // Interrupt called. New tasks cannot be added
-                return false;
-            }
-
-            // Wait notification from getAndPop() or notifyAll()
-            _queueFullCondVar.wait(lock);
-        }
-
-        // TODO: Check
-        _taskQueue.emplace(std::forward<Task>(task));
-        lock.unlock();
-        // Notify thread holded into getAndPop()
-        _queueEmptyCondVar.notify_one();
-
-        return true;
-    }
-
+    bool emplace(Task&& task, const bool& workFlag);
     // Thread-safely gets front element and pops the queue
-    Task getAndPop(const bool& workFlag)
-    {
-        std::unique_lock<std::mutex> lock(_queueMtx);
-
-        // Hold thread if there are no tasks available
-        while(_taskQueue.empty())
-        {
-            if(!workFlag)
-            {
-                // Interrupt called
-                return nullptr;
-            }
-
-            // Wait notification from getAndPop() or notifyAll()
-            _queueEmptyCondVar.wait(lock);
-        }
-
-        // TODO: Check
-        Task val = move(_taskQueue.front());
-        _taskQueue.pop();
-        lock.unlock();
-        // Notify thread holded into emplace()
-        _queueFullCondVar.notify_one();
-
-        return val;
-    }
+    Task getAndPop(const bool& workFlag);
 
 private:
     // Task queue
@@ -91,3 +35,64 @@ private:
     // Queue max size limiter
     static constexpr int _queueMaxSize = std::numeric_limits<int>::max();
 };
+
+template<typename Task>
+void Buffer<Task>::notifyAll()
+{
+    _queueEmptyCondVar.notify_all();
+    _queueFullCondVar.notify_all();
+}
+
+template<typename Task>
+bool Buffer<Task>::emplace(Task&& task, const bool& workFlag)
+{
+    std::unique_lock<std::mutex> lock(_queueMtx);
+
+    // Hold thread if task limit had been reached
+    while(_taskQueue.size() >= _queueMaxSize)
+    {
+        // Interrupt operation if work flag is false
+        if(!workFlag)
+        {
+            // Interrupt called. New tasks cannot be added
+            return false;
+        }
+
+        // Wait notification from getAndPop() or notifyAll()
+        _queueFullCondVar.wait(lock);
+    }
+
+    _taskQueue.emplace(std::forward<Task>(task));
+    lock.unlock();
+    // Notify thread holded into getAndPop()
+    _queueEmptyCondVar.notify_one();
+
+    return true;
+}
+
+template<typename Task>
+Task Buffer<Task>::getAndPop(const bool& workFlag)
+{
+    std::unique_lock<std::mutex> lock(_queueMtx);
+
+    // Hold thread if there are no tasks available
+    while(_taskQueue.empty())
+    {
+        if(!workFlag)
+        {
+            // Interrupt called
+            return nullptr;
+        }
+
+        // Wait notification from getAndPop() or notifyAll()
+        _queueEmptyCondVar.wait(lock);
+    }
+
+    Task val = move(_taskQueue.front());
+    _taskQueue.pop();
+    lock.unlock();
+    // Notify thread holded into emplace()
+    _queueFullCondVar.notify_one();
+
+    return val;
+}
